c24702d66b346d257f0f45a99973d8b6c5373096
Ashok2511 <ashok250411@gmail.com>
2023-07-08 00:37:26 +0530

    production 7th

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+ const CONNECTION_STRING = "mongodb+srv://ashokja9658:ashok9957@cluster0.pun52t1.mongodb.net/mycontacts-backends?retryWrites=true&w=majority"
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    const connect = await mongoose.connect(CONNECTION_STRING);
     console.log(
       "Database connected:",
       connect.connection.host,
