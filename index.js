6fadc9e9957781a084aa95332158f47c82ebac03
Ashok2511 <ashok250411@gmail.com>
2023-07-16 01:52:31 +0530

    Ashok vercel 10

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://bookshop-livid.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 // app.use(
