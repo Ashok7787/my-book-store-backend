60c9083749b734e9f1edb2f708347726b281eafc
Ashok2511 <ashok250411@gmail.com>
2023-07-16 01:45:16 +0530

    Ashok vercel 7

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const dotenv = require("dotenv").config();
 connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://bookshop-livid.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(
     cors(
       {
